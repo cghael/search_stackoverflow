0f705fce3da5f3cc8e1915125104a3cd7ab86151
Anton Gorobets <a.p.gorobets@gmail.com>
2022-04-06 16:38:16 +0300

    Host Exception handle

--- src/stacksearch/api.clj
+++ src/stacksearch/api.clj
@@ -2,12 +2,13 @@
   (:require
     [cheshire.core :refer :all]
     [clj-http.client :as client]
-    [com.climate.claypoole :as cp]))
+    [com.climate.claypoole :as cp]
+    [clojure.pprint :as pp]))
 
 
 (defn query-params
   [tag]
-  {"pagesize" "100"
+  {"pagesize" "1"
    "order"    "desc"
    "sort"     "creation"
    "tagged"   tag
@@ -29,13 +30,16 @@
 (defn get-parallel-search
   "Sends request to the server, processes the result"
   [tag]
-  (let [url        (or (System/getenv "SOURCE_URL") "https://api.stackexchange.com/2.3/search")
-        res        (client/get url {:query-params (query-params tag)})
-        res-status (:status res)]
-    (if (= res-status 200)
-      (let [res-body (parse-string (:body res) true)]
-        (get-tag-statistic (keyword tag) (:items res-body)))
-      (swap! statistic assoc (keyword tag) {:error "server unavailable"}))))
+  (try
+    (let [url        (or (System/getenv "SOURCE_URL") "https://api.stackexchange.com/2.3/search")
+          res        (client/get url {:query-params (query-params tag)})
+          res-status (:status res)]
+      (if (= res-status 200)
+        (let [res-body (parse-string (:body res) true)]
+          (get-tag-statistic (keyword tag) (:items res-body)))
+        (swap! statistic assoc (keyword tag) {:error "Server unavailable"})))
+    (catch Exception e
+      (swap! statistic assoc (keyword tag) {:error "HostException. Server unavailable"}))))
 
 
 (defn search
